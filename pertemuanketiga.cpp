d35eb795d63e4cb7cfb17de98ed0c080c7f437b3
LENOVO <LENOVO@LAPTOP-AI2PIVR2>
2024-03-08 10:58:54 +0700

    Add project files.

---
+++ pertemuanketiga.cpp
@@ -0,0 +1,12 @@
+// pertemuanketiga.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+{
+    std::cout << "Hello World!\n";
+}
+
